984738abba7db1b2da8741b8e4700e95b574f799
vaibhav6900 <starryvaibh786@gmail.com>
2022-09-14 21:40:03 +0530

    initial

---
+++ app.js
@@ -0,0 +1,32 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+let items=["buy","ccc"];
+
+app.get("/",function(req,res){
+    let today=new Date();
+   let options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"  
+   }
+   let day=today.toLocaleDateString("en-US",options);
+    
+    res.render("list",{kindOfDay:day, newListItems:items});
+})
+
+app.post("/",function(req,res){
+      let item= req.body.newItem;
+      items.push(item);
+     console.log(item);
+     res.redirect("/");
+})
+
+
+app.listen(3000,function(){
+    console.log("server started on port 3000");
+})
